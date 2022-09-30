df4c9b0c68f625989df391b73c8252767184adac
Mallejac <jean.mallejac@gmail.com>
2022-09-30 16:17:03 +0200

    code complet 2

--- metrics.py
+++ metrics.py
@@ -5,6 +5,21 @@ import spectral
 from skimage import metrics
 
 def SSIM(IA,IB):
+    r"""
+    Structural similarity index measure implementation between image A and image B  
+
+    Parameters
+    ----------
+    IA : array-like, shape (l,p)
+         image A
+    IB : array-like, shape (l,p)
+         image B
+    Returns
+    -------
+    value_SSIM : array-like, shape (l,1)
+                 SSIM values for each spectral bands of image A and image B 
+
+    """
     Ia = Ip.mat2im(IA)
     Ib = Ip.mat2im(IB)
 
@@ -15,6 +30,21 @@ def SSIM(IA,IB):
     return value_SSIM
 
 def SAM(Xp,X):
+    r"""
+    Spectral Angle Mapper implementation between image Xp and image X 
+
+    Parameters
+    ----------
+    X : array-like, shape (l,p)
+         image obtained
+    Xp : array-like, shape (l,p)
+         image of reference
+    Returns
+    -------
+    img_SAM : array-like, shape (\sqrt(p),\sqrt(p))
+              image SAM with the Spectral Angle between image X and Xp for each pixel 
+
+    """
     ne = np.int(np.sqrt(Xp.shape[1]))
     pix = np.zeros(ne**2)
     X_img = Ip.mat2im(X)
